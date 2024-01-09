be8c0822f865821c9b68bddab942739acdfa684d
Hemasree01 <onmanihemasree@gmail.com>
2024-01-09 11:43:36 -0500

    new

--- examples.js
+++ examples.js
@@ -32,24 +32,80 @@ console.log(calcAge(year[1]));
 console.log(calcAge(year[year.length - 1]));
 const ages = [calcAge(year[1]), calcAge(year[year.length - 1])];
 console.log(ages);
-// challenge 2b
-const calcTip = function (bill) {
-  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-};
-console.log(calcTip(100));
-const bills = [125, 555, 44];
-const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
-console.log(bills, tips);
 
-const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
-console.log(bills, tips, totals);
-// challenge 3
+// challenge 2b
+// const calcTip = function (bill) {
+//   return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// };
+// console.log(calcTip(100));
+// const bills = [125, 555, 44];
+// const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+// console.log(bills, tips);
+
+// const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]];
+// console.log(bills, tips, totals);
+// challenge 3a
+//objects examples
 const jonas = {
   firstName: "jonas",
   lastName: "trump",
   age: 2037 - 1991,
   job: "programmer",
   friends: ["Michael", "Peter", "Steven"],
+  heHasADriversLicsence: false,
+  driverLis: function () {
+    return `${this.heHasADriversLicsence ? "a" : "no"}`;
+  },
 };
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his best friend is
-called ${jonas.friends[0]}`);
+console.log(`${jonas.firstName} is ${jonas.age}-years old ${jonas.job}, and he 
+has ${jonas.driverLis()} drivers license`);
+//object
+//challenge 3b
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+
+if (mark.bmi > john.bmi) {
+  console.log(
+    `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`
+  );
+} else if (john.bmi > mark.bmi) {
+  console.log(
+    `${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`
+  );
+}
+//for loop challenge 4
+const calcTip = function (bill) {
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
+
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(calcTip(bills[i]));
+  totals.push(tip + bills[i]);
+}
+console.log(`bills: ${bills}
+  tips: ${tips}
+  total: ${totals}`);
--- script.js
+++ script.js
@@ -1,7 +1,22 @@
 "use strict";
 import School from "./School.js";
 
-const everydayPack = new School("Everyday Backpack", 30, 15, false);
+const everydayPack = new School("GRADES OF A STUDENT", 30, 15, false);
+
+const content = `
+<main>
+      <article>
+        <h1>${everydayPack.name}</h1>
+        <ul>
+          <li>math score:${everydayPack.subjects.subject1}</li>
+          <li>chemistry score:${everydayPack.subjects.subject2}</li>
+          <li>gate status:${everydayPack.gateOpen}</li>
+        </ul>
+      </article>
+    </main>
+
+`;
+document.body.innerHTML = content;
 
 console.log("The everydayPack object:", everydayPack);
 console.log("The pocketNum value:", everydayPack.name);
